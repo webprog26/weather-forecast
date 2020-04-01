15247bf8f372281a69c2fe2bf0191ada72ff3054
webprog26 <webprog26@gmail.com>
2020-04-01 16:22:24 +0300

    Location detection added

--- app/src/main/java/com/webprog26/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/webprog26/weatherforecast/MainActivity.kt
@@ -1,14 +1,120 @@
 package com.webprog26.weatherforecast
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.gms.common.GooglePlayServicesUtil
+import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.snackbar.Snackbar
+import com.webprog26.weatherforecast.databinding.ActivityMainBinding
+import java.util.*
 
+const val ACCESS_COARSE_LOCATION_PERMISSION_REQUEST_CODE = 0
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
+        val viewModelFactory = ViewModelFactory(application)
+
+        mainViewModel = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
+        mainViewModel.userLocation.observe(this, Observer {
+            if (it != null) {
+                log("Location received. latitude: ${it.latitude}, longitude: ${it.longitude}")
+                binding.progressBar.visibility = View.VISIBLE
+                binding.tvMessage.visibility = View.VISIBLE
+                binding.tvMessage.text = getString(R.string.detecting_user_location_text)
+                mainViewModel.onUserLocationDetected(getString(R.string.accuweather_api_key),
+                    "${it.latitude}, ${it.longitude}", Locale.getDefault().language,
+                "false", "true")
+            } else {
+                log("Location is null")
+            }
+        })
+        mainViewModel.currentCity.observe(this, Observer {
+            it?.let {
+                binding.tvMessage.text = getString(R.string.loading_forecast_text, it);
+            }
+        })
+
+        checkLocationPermissionIfNeededAndProceed()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        log("onRestart")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION),
+                    ACCESS_COARSE_LOCATION_PERMISSION_REQUEST_CODE
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            ACCESS_COARSE_LOCATION_PERMISSION_REQUEST_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    initLocationFetching()
+                } else {
+                    binding.tvMessage.text = getString(R.string.location_permission_request_explanation)
+                }
+                return
+            }
+        }
+
+    }
+
+    private fun checkLocationPermissionIfNeededAndProceed() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION),
+                    ACCESS_COARSE_LOCATION_PERMISSION_REQUEST_CODE
+                )
+
+            } else {
+                initLocationFetching()
+            }
+        } else {
+            initLocationFetching()
+        }
+    }
+
+    private fun initLocationFetching() {
+        log("initLocationFetching")
+        mainViewModel.detectUserLocation()
     }
 }
---
+++ app/src/main/java/com/webprog26/weatherforecast/network/WeatherApiService.kt
@@ -0,0 +1,39 @@
+package com.webprog26.weatherforecast.network
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import kotlinx.coroutines.Deferred
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+private const val BASE_URL = "http://dataservice.accuweather.com"
+
+val httpLoggingInterceptor = HttpLoggingInterceptor()
+val httpClient = OkHttpClient.Builder()
+
+private val retrofit = Retrofit.Builder()
+    .baseUrl(BASE_URL)
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .client(httpClient.addInterceptor(httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)).build())
+    .build()
+
+object WeatherApi {
+    val retrofitService : WeatherApiService by lazy {
+        retrofit.create(WeatherApiService::class.java)
+    }
+}
+
+interface WeatherApiService {
+
+    @GET("/locations/v1/cities/geoposition/search")
+    fun getLocationDataAsync(@Query("apikey") apiKey: String,
+                             @Query("q") coordinates: String,
+                             @Query("language") language: String,
+                             @Query("details") details: String,
+                             @Query("topLevel") topLevel: String): Deferred<String>
+}
