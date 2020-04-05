549a6782f5e64d2263eb70ed55d7f7787e010686
Volodymyr Shalashenko <webprog26@gmail.com>
2020-04-05 15:36:32 +0300

    Daily forecast loading added (#2)

--- app/src/main/java/com/webprog26/weatherforecast/network/WeatherApiService.kt
+++ app/src/main/java/com/webprog26/weatherforecast/network/WeatherApiService.kt
@@ -8,6 +8,7 @@ import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 private const val BASE_URL = "http://dataservice.accuweather.com"
@@ -19,11 +20,14 @@ private val retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
     .addConverterFactory(ScalarsConverterFactory.create())
     .addCallAdapterFactory(CoroutineCallAdapterFactory())
-    .client(httpClient.addInterceptor(httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)).build())
+    .client(
+        httpClient.addInterceptor(httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
+    )
     .build()
 
 object WeatherApi {
-    val retrofitService : WeatherApiService by lazy {
+    val retrofitService: WeatherApiService by lazy {
         retrofit.create(WeatherApiService::class.java)
     }
 }
@@ -31,9 +35,20 @@ object WeatherApi {
 interface WeatherApiService {
 
     @GET("/locations/v1/cities/geoposition/search")
-    fun getLocationDataAsync(@Query("apikey") apiKey: String,
-                             @Query("q") coordinates: String,
-                             @Query("language") language: String,
-                             @Query("details") details: String,
-                             @Query("topLevel") topLevel: String): Deferred<String>
+    fun getLocationDataAsync(
+        @Query("apikey") apiKey: String,
+        @Query("q") coordinates: String,
+        @Query("language") language: String,
+        @Query("details") details: String,
+        @Query("topLevel") topLevel: String
+    ): Deferred<String>
+
+    @GET("/forecasts/v1/daily/1day/{location_key}")
+    fun getDailyForecastAsync(
+        @Path("location_key") locationKey: String,
+        @Query("apikey") apiKey: String,
+        @Query("language") language: String,
+        @Query("details") details: String,
+        @Query("metric") metric: String
+    ): Deferred<String>
 }
